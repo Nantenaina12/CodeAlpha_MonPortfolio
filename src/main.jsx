1bbabc2a0572bfa44060918d2168fd7d6bc93099
Orlando <nantenainaorlando1@gmail.com>
2025-08-12 01:27:33 +0100

    Je viens de créer les routes de mon application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter,RouterProvider, } from 'react-router-dom';
+import './index.css';
 import App from './App.jsx'
+
+//pages
+import Home from './pages/Home.jsx';;
+import About from './pages/About.jsx';
+import Projects from './pages/Projects.jsx';
+import Expérience from './pages/Expérience.jsx';
+import Contact from './pages/Contact.jsx';
+
+// Création des routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Layout global
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/projects", element: <Projects /> },
+      { path: "/experience", element: <Expérience /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/pages/Expérience.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Expérience() {
+  return (
+    <div>Expérience</div>
+  )
+}
