f9ffc73dfa9171056de7eb56a6b1d4e789b3ccdc
Orlando <nantenainaorlando1@gmail.com>
2025-08-15 15:43:12 +0100

    J'ai juste modifié les routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter,RouterProvider, } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, createRoutesFromChildren, Route, Routes } from 'react-router-dom';
 import './index.css';
-import App from './App.jsx'
+import App from './App.jsx';
 
-//pages
-import Home from './pages/Home.jsx';;
+// Pages
+import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
 import Expérience from './pages/Expérience.jsx';
 import Contact from './pages/Contact.jsx';
 
-// Création des routes
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />, // Layout global
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/projects", element: <Projects /> },
-      { path: "/experience", element: <Expérience /> },
-      { path: "/contact", element: <Contact /> },
-    ],
-  },
-])
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="experience" element={<Expérience />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
